3770e4177574713e0a7d14a52aa6e22cdf54d589
mrartur0074 <smiteartur01@mail.ru>
2022-11-22 09:21:06 +0600

    create category and basket

--- javascript/category.js
+++ javascript/category.js
@@ -1,5 +1,5 @@
 /* Вывод всех категорий товаров */
-const categories = query('https://63720b6d078587786188f1b3.mockapi.io/api/v1/category').then(json => {
+/*const categories = query('https://63720b6d078587786188f1b3.mockapi.io/api/v1/category').then(json => {
     let div_categories = document.querySelector('.categories');
     json.forEach(element => {
         let category = document.createElement('div');
@@ -7,4 +7,4 @@ const categories = query('https://63720b6d078587786188f1b3.mockapi.io/api/v1/cat
         category.innerHTML = `<a href="category.html?category=${element.id}">${element.name}</a>`;
         div_categories.append(category);
     });
-});
+});*/
